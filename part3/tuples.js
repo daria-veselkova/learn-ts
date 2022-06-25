05989280c6da9e5cc42a280789455a6ad5463336
Daria Veselkova <daria.veselkova@yandex.ru>
2022-06-25 15:23:09 +0300

    save other exrcises

---
+++ part3/tuples.js
@@ -0,0 +1,7 @@
+var skill = [1, 'dev'];
+var skill2 = [2, 'ops'];
+var id = skill2[0];
+var skillName = skill2[1];
+var arr = [1, 'dep', true, false];
+var skill3 = [3, 'front'];
+var skill4 = ['end', 'back'];
